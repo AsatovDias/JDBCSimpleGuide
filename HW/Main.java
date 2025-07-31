2fb0032b9ad309cb7a1eb33cf7193ef0c324a0a6
AsatovDias <d_assat@kbtu.kz>
2025-07-31 23:37:58 +0500

    Update Main.java

--- HW/Main.java
+++ HW/Main.java
@@ -8,21 +8,32 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Connector db = new Connector();
-
+        db.connect();
         System.out.println("Enter your card number: ");
         int cardNum = sc.nextInt();
-        User user = db.getUser(cardNum);
-        if(user != null){
+        System.out.print("Enter your pinCode: ");
+        int pinCode = sc.nextInt();
+
+        if(db.auth(cardNum, pinCode)) {
+            User user =  db.getUser(cardNum);
             System.out.println("Hello " + user.getName() + " " + user.getSurname());
-            while(true){
+            while(true) {
                 System.out.println("[1] Balance");
                 System.out.println("[2] Cash IN");
                 System.out.println("[3] Cash OUT");
                 System.out.println("[0] exit");
-
+                int choice = sc.nextInt();
+                if (choice == 1) {
+                    System.out.println(db.getTotalBalance(cardNum, pinCode));
+                }else {
+                    break;
+                }
             }
+        }else{
+            System.out.println("Invalid PinCode");
         }
 
     }
 
 }
+
